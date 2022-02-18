45274ed1fd7c4dc446f81656758a417d86054c63
Avishay Zamir <avishayzamir@Avishays-MacBook-Pro.local>
2022-02-18 18:19:38 +0200

    fixes

--- payment/processor/processor.go
+++ payment/processor/processor.go
@@ -1,7 +1,7 @@
 package processor
 
 import (
-	b64 "encoding/base64"
+	"encoding/base64"
 	"payment/data"
 	"strings"
 	"sync"
@@ -18,12 +18,12 @@ type panProcessor struct {
 }
 
 func (p *panProcessor) Encode(transaction data.Transaction) {
-	encode := b64.StdEncoding.EncodeToString([]byte(transaction.GetCard().GetPan()))
+	encode := base64.StdEncoding.EncodeToString([]byte(transaction.GetCard().GetPan()))
 	transaction.GetCard().SetPan(encode)
 }
 
 func (p *panProcessor) Decode(transaction data.Transaction) {
-	decode, _ := b64.StdEncoding.DecodeString(transaction.GetCard().GetPan())
+	decode, _ := base64.StdEncoding.DecodeString(transaction.GetCard().GetPan())
 	//TODO check err
 	value := string(decode)
 	l := len(value)
@@ -35,12 +35,12 @@ type expiryDateProcessor struct {
 }
 
 func (e expiryDateProcessor) Encode(transaction data.Transaction) {
-	encode := b64.StdEncoding.EncodeToString([]byte(transaction.GetCard().GetExpiry()))
+	encode := base64.StdEncoding.EncodeToString([]byte(transaction.GetCard().GetExpiry()))
 	transaction.GetCard().SetExpiry(encode)
 }
 
 func (e expiryDateProcessor) Decode(transaction data.Transaction) {
-	decode, _ := b64.StdEncoding.DecodeString(transaction.GetCard().GetExpiry())
+	decode, _ := base64.StdEncoding.DecodeString(transaction.GetCard().GetExpiry())
 	//TODO check err
 	l := len(string(decode))
 	value := strings.Repeat("*", l)
@@ -51,12 +51,12 @@ type cardHolderProcessor struct {
 }
 
 func (p *cardHolderProcessor) Encode(transaction data.Transaction) {
-	encode := b64.StdEncoding.EncodeToString([]byte(transaction.GetCardHolder().GetName()))
+	encode := base64.StdEncoding.EncodeToString([]byte(transaction.GetCardHolder().GetName()))
 	transaction.GetCardHolder().SetName(encode)
 }
 
 func (p *cardHolderProcessor) Decode(transaction data.Transaction) {
-	decode, _ := b64.StdEncoding.DecodeString(transaction.GetCardHolder().GetName())
+	decode, _ := base64.StdEncoding.DecodeString(transaction.GetCardHolder().GetName())
 	//TODO check err
 	l := len(string(decode))
 	value := strings.Repeat("*", l)
