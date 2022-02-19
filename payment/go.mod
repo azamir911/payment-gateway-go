07c94acb63bbf8d93c7b6d68aa4d628be6c8cd07
Avishay Zamir <avishayzamir@Avishays-MacBook-Pro.local>
2022-02-19 17:15:06 +0200

    added tests

--- payment/go.mod
+++ payment/go.mod
@@ -3,6 +3,10 @@ module payment
 go 1.17
 
 require (
+	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/qodrorid/godaemon v0.0.0-20170331035433-aeac1c057d11 // indirect
 	github.com/rs/zerolog v1.26.1 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
---
+++ payment/processor/processor_test.go
@@ -0,0 +1,44 @@
+package processor
+
+import (
+	"github.com/stretchr/testify/assert"
+	"payment/data"
+	"testing"
+)
+
+func TestPanProcessor(t *testing.T) {
+	tr := data.NewTransaction(1234567, -10, "EUR", "First Last", "email@domain.com", "4188846122476411", "0624")
+
+	p := &panProcessor{}
+	p.Encode(*tr)
+
+	assert.Equalf(t, "First Last", (*tr).GetCardHolder().GetName(), "name")
+	assert.Equalf(t, "NDE4ODg0NjEyMjQ3NjQxMQ==", (*tr).GetCard().GetPan(), "Pan should be encrypted")
+
+	p.Decode(*tr)
+	assert.Equalf(t, "************6411", (*tr).GetCard().GetPan(), "Pan should be mask all characters, except last 4 digits")
+}
+
+func TestCardHolderProcessor(t *testing.T) {
+	tr := data.NewTransaction(1234567, -10, "EUR", "First Last", "email@domain.com", "4188846122476411", "0624")
+
+	p := &cardHolderProcessor{}
+	p.Encode(*tr)
+
+	assert.Equalf(t, "Rmlyc3QgTGFzdA==", (*tr).GetCardHolder().GetName(), "Name should be encrypted")
+
+	p.Decode(*tr)
+	assert.Equalf(t, "**********", (*tr).GetCardHolder().GetName(), "Name should be mask all characters")
+}
+
+func TestExpiryDateProcessor(t *testing.T) {
+	tr := data.NewTransaction(1234567, -10, "EUR", "First Last", "email@domain.com", "4188846122476411", "0624")
+
+	p := &expiryDateProcessor{}
+	p.Encode(*tr)
+
+	assert.Equalf(t, "MDYyNA==", (*tr).GetCard().GetExpiry(), "Expiry date should be encrypted")
+
+	p.Decode(*tr)
+	assert.Equalf(t, "****", (*tr).GetCard().GetExpiry(), "Expiry date should be mask all characters")
+}
